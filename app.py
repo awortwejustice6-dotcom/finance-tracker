f1a92c730f9c703434ee7b39720bcd281cb7e47c
awortwejustice6-dotcom <awortwejustice6@gmail.com>
2026-03-11 17:25:42 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -129,9 +129,8 @@ def main():
 # --- HIDE STREAMLIT BRANDING & MENU ---
     hide_st_style = """
             <style>
-         
+            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: hidden;}
             </style>
             """
     st.markdown(hide_st_style, unsafe_allow_html=True)
